edf936675faeb5a939477102e7533b9d6907022f
carymak <carymaksy@gmail.com>
2023-11-10 22:40:31 +0900

    first commit

---
+++ 2-global.js
@@ -0,0 +1,15 @@
+function sayHello(name) {
+    console.log('Hello ' + name)
+}
+
+
+// Global   - no window
+// __dirname    - path to current directory
+// __filename   - file name
+// require      - function to use module (CommonJS)
+// module       - info about current module (file)
+// process      - info about env where the program is being executed
+
+setInterval(() =>  {
+    console.log('hello world')
+}, 1000)
---
+++ app.js
@@ -0,0 +1,14 @@
+// local
+// npm i <packageName>
+
+// global
+// npm install -g <packageName>
+
+// package.json - manifest file
+// manual approach (create package.json in the root)
+// npm init (step by step)
+// npm init -y (everything default)
+
+// npm install (install all package in package.json dependencies)
+
+// npm i <packageName> -D (dev dependencies)
